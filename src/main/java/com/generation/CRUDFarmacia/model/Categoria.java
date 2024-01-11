bb1f88ebc3611ca25018c4b2cff96bd56a22cf5f
SabrinaLeonel <94725427+SabrinaLeonel@users.noreply.github.com>
2024-01-11 04:07:49 -0300

    CRUD-Categoria

--- src/main/java/com/generation/CRUDFarmacia/model/Categoria.java
+++ src/main/java/com/generation/CRUDFarmacia/model/Categoria.java
@@ -1,22 +1,25 @@
 package com.generation.CRUDFarmacia.model;
 
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
 
-import org.hibernate.annotations.UpdateTimestamp;
+import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import java.time.LocalDate;
 
-@Entity
-@Table(name = "tb_categorias")
-public class Categoria {
+
+	@Entity
+	@Table(name = "tb_categorias")
+	public class Categoria {
     
     @Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY) 
@@ -30,16 +33,9 @@ public class Categoria {
 	@Size(min = 10, max = 500, message = "O atributo descricao deve conter no mínimo 10 e no máximo 500 caracteres")
 	private String descricao;
 
-	private BigDecimal precoMedio;
-
-	@NotBlank(message = "O atributo tipo é Obrigatório!")
-	@Size(min = 3, max = 50, message = "O atributo tipo deve conter no mínimo 3 e no máximo 50 caracteres")
-	private String tipo;
-
-	@UpdateTimestamp
-	private LocalDateTime dataCadastro;
-	
-	private LocalDate dataValidade;
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "categoria", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("categoria")
+	private List<Produto> produto;
 
     public Long getId() {
         return this.id;
@@ -64,36 +60,14 @@ public class Categoria {
     public void setDescricao(String descricao) {
         this.descricao = descricao;
     }
-
-    public BigDecimal getPrecoMedio() {
-        return this.precoMedio;
-    }
-
-    public void setPrecoMedio(BigDecimal precoMedio) {
-        this.precoMedio = precoMedio;
-    }
-
-    public String getTipo() {
-        return this.tipo;
-    }
-
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
-    }
-
-    public LocalDateTime getDataCadastro() {
-        return this.dataCadastro;
-    }
-
-    public void setDataCadastro(LocalDateTime dataCadastro) {
-        this.dataCadastro = dataCadastro;
-    }
     
-    public LocalDate getDataValidade() {
-        return this.dataValidade;
-    }
+	public List<Produto> getProduto() {
+		return produto;
+	}
 
-    public void setDataValidade(LocalDate dataValidade) {
-        this.dataValidade = dataValidade;
-    }    
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
+	}
+
+ 
 }
