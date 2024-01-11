9d8bbe33440586e994f2d6bea347509bc223723f
SabrinaLeonel <sabrinaleonel12@gmail.com>
2024-01-11 03:46:07 -0300

    CRUD-Produto

---
+++ src/main/java/com/generation/CRUDFarmacia/model/Produto.java
@@ -0,0 +1,116 @@
+package com.generation.CRUDFarmacia.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDate;
+
+	@Entity
+	@Table(name = "tb_produtos")
+	public class Produto {
+	    
+    @Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) 
+	private Long id;
+	
+	@NotBlank(message = "O atributo nome é Obrigatório!") 
+	@Size(min = 5, max = 100, message = "O atributo nome deve conter no mínimo 05 e no máximo 100 caracteres")
+	private String nome;
+	
+	@NotBlank(message = "O atributo descricao é Obrigatório!")
+	@Size(min = 10, max = 500, message = "O atributo descricao deve conter no mínimo 10 e no máximo 500 caracteres")
+	private String descricao;
+
+	private BigDecimal precoMedio;
+
+	@NotBlank(message = "O atributo tipo é Obrigatório!")
+	@Size(min = 3, max = 50, message = "O atributo tipo deve conter no mínimo 3 e no máximo 50 caracteres")
+	private String tipo;
+
+	@UpdateTimestamp
+	private LocalDateTime dataCadastro;
+	
+	private LocalDate dataValidade;
+	
+    @ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return this.descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public BigDecimal getPrecoMedio() {
+        return this.precoMedio;
+    }
+
+    public void setPrecoMedio(BigDecimal precoMedio) {
+        this.precoMedio = precoMedio;
+    }
+
+    public String getTipo() {
+        return this.tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public LocalDateTime getDataCadastro() {
+        return this.dataCadastro;
+    }
+
+    public void setDataCadastro(LocalDateTime dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+    
+    public LocalDate getDataValidade() {
+        return this.dataValidade;
+    }
+
+    public void setDataValidade(LocalDate dataValidade) {
+        this.dataValidade = dataValidade;
+    }
+    
+    public Categoria getCategoria() {
+        return this.categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+}
